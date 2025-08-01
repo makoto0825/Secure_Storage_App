92f5f64225653972f52bc0b57a2b46cc3c3df647
makoto082 <makototsuga@gmail.com>
2025-07-31 20:38:40 -0400

    レイアウト修正

--- frontend/src/app/download/page.tsx
+++ frontend/src/app/download/page.tsx
@@ -64,6 +64,7 @@ const DownloadPage = () => {
               <button
                 onClick={() => handleDownload(file)}
                 style={{ marginLeft: '1rem' }}
+                className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200'
               >
                 ダウンロード
               </button>
